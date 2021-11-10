503aa4d7967602a1a252eb3ef15e1b827fd07773
Erik Brown <erik@idxbroker.com>
2021-11-09 23:12:26 -0800

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(indexRouter);
 app.use(porfolioRouter);
-// app.use(editRouter);
+app.use(editRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
